166e478bf46775be1a92f71795e4006e9a005e6e
alesancor1 <alesancor1@alum.us.es>
2022-05-07 21:15:37 +0200

    feat: Implemented SLA Rate Limit module

---
+++ src/sla-rate-limit/index.js
@@ -0,0 +1,20 @@
+import { OASBase } from "oas-devtools/middleware";
+import rateLimit from "express-rate-limit";
+import slowDown from "express-slow-down";
+
+export class SLARateLimit extends OASBase {
+    constructor(oasFile, middleware) {
+        super(oasFile, middleware);
+    }
+
+    static initialize(oasDoc, config) {
+        const limiter = rateLimit(config.rateLimit);
+        const speedLimiter = slowDown(config.speedLimit);
+
+        return SLARateLimit(oasDoc, (req, res, next) => {
+            speedLimiter(req, res, () => {
+                limiter(req, res, next);
+            });
+        });
+    }
+}

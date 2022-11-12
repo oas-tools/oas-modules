566205b47aa1368a01dd94ed4811c0ca58e56812
alesancor1 <alesancor1@alum.us.es>
2022-11-12 17:47:55 +0100

    feat: Implemented apikey based rate limiting

--- src/index.js
+++ src/index.js
@@ -1,62 +1,38 @@
 import $RefParser from "@apidevtools/json-schema-ref-parser";
-import { getConfig } from "./utils/index.js";
-import { OASBase } from "@oas-tools/commons";
-import rateLimit from "express-rate-limit";
-import slowDown from "express-slow-down";
+import { logger, OASBase } from "@oas-tools/commons";
+import metaStore from "./utils/store.js";
 
 export class SLARateLimit extends OASBase {
-  #rateLimitCfg;
 
-  #slowDownCfg;
-
-  constructor(oasFile, rateLimitCfg, slowDownCfg) {
-    super(oasFile);
-    this.#rateLimitCfg = rateLimitCfg;
-    this.#slowDownCfg = slowDownCfg;
+  constructor(oasFile, middleware) {
+    super(oasFile, middleware);
   }
 
   static async initialize(oasDoc, config) {
     const slaFile = await $RefParser.dereference(
       config.slaFile ?? `${process.cwd()}/api/oas-sla.yaml`
     );
-    const plan = slaFile.plans[config.currentPlan ?? "base"];
+    
+    const store = new metaStore(slaFile, config);
+    
+    const middleware = (req, res, next) => {
+      let plan = res.locals.oas?.security?.[config.scheme]?.plan;
+      if (!slaFile.plans[plan]) {
+        logger.warn(`plan ${plan} is not defined in the SLA.`);
+        plan = Object.keys(slaFile.plans)[0];
+      }
+      logger.info(`Using "${plan}" plan`);
 
-    const rateLimitCfg = plan.quotas ? getConfig(plan, config, "quotas") : null;
-    const slowDownCfg = plan.rates ? getConfig(plan, config, "rates") : null;
+      const endpoint = `${req.method}-${req.path}`;
+      const key = `${req.ip}-${plan}-${endpoint}`;
+      
+      let limiters = store.get(key);
+      if (!limiters) limiters = store.add(key);
 
-    return new SLARateLimit(oasDoc, rateLimitCfg, slowDownCfg);
-  }
-
-  register(app) {
-    // Register for endpoints with rates and quotas defined
-    if (this.#slowDownCfg) {
-      Object.entries(this.#slowDownCfg).forEach(([endpoint, slowDownConf]) => {
-        let middleware;
-        const speedLimiter = slowDown(slowDownConf);
-        if (this.#rateLimitCfg?.[endpoint]?.operation === slowDownConf.operation) {
-          const limiter = rateLimit(this.#rateLimitCfg[endpoint]);
-          middleware = (req, res, next) =>
-            speedLimiter(req, res, () => limiter(req, res, next));
-        } else {
-          middleware = speedLimiter;
-        }
-        app[slowDownConf.operation](endpoint, middleware);
-      });
+      const {speedLimiter, rateLimiter} = limiters;
+      speedLimiter(req, res, () => rateLimiter(req, res, next));
     }
 
-    // Register for endpoints with only quotas defined
-    if (this.#rateLimitCfg) {
-      Object.entries(this.#rateLimitCfg)
-        .filter(
-          ([endpoint, rateLimitConf]) =>
-            this.#slowDownCfg?.[endpoint]?.operation !== rateLimitConf.operation
-        )
-        .forEach(([endpoint, rateLimitConf]) => {
-          const limiter = rateLimit(rateLimitConf);
-          app[rateLimitConf.operation](endpoint, (req, res, next) =>
-            limiter(req, res, next)
-          );
-        });
-    }
+    return new SLARateLimit(oasDoc, middleware);
   }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,41 +2,42 @@ import _ from "lodash";
 
 export function getConfig(plan, config, scopeType) {
   return Object.entries(plan[scopeType]).reduce((acc, [endp, opObject]) => {
-    acc[endp] = {};
+    acc = {};
     Object.entries(opObject).forEach(([op, scope]) => {
+      const endpoint = `${op.toUpperCase()}-${expressPath(endp)}`;
       const scopeSla = scope[config.requestIdentifier ?? "requests"];
       const period = scopeSla[0].period;
-      acc[endp].operation = op;
-      acc[endp].max = scopeSla[0].max ?? 0;
+      acc[endpoint] = {};
+      acc[endpoint].max = scopeSla[0].max ?? 1;
       switch (period) {
         case "second":
-          acc[endp].windowMs = 1000;
+          acc[endpoint].windowMs = 1000;
           break;
         case "minute":
-          acc[endp].windowMs = 60 * 1000;
+          acc[endpoint].windowMs = 60 * 1000;
           break;
         case "hour":
-          acc[endp].windowMs = 60 * 60 * 1000;
+          acc[endpoint].windowMs = 60 * 60 * 1000;
           break;
         case "day":
-          acc[endp].windowMs = 24 * 60 * 60 * 1000;
+          acc[endpoint].windowMs = 24 * 60 * 60 * 1000;
           break;
         case "month":
-          acc[endp].windowMs = 30 * 24 * 60 * 60 * 1000;
+          acc[endpoint].windowMs = 30 * 24 * 60 * 60 * 1000;
           break;
         case "year":
-          acc[endp].windowMs = 365 * 24 * 60 * 60 * 1000;
+          acc[endpoint].windowMs = 365 * 24 * 60 * 60 * 1000;
           break;
       }
       if (scopeType === "rates") {
-        acc[endp].delayMs = acc[endp].windowMs / acc[endp].max;
-        acc[endp].windowMs = 60000;
+        acc[endpoint].delayMs = acc[endpoint].windowMs / acc[endpoint].max;
+        acc[endpoint].maxDelayMs = acc[endpoint].delayMs;
       }
     });
-    acc[endp] = _.merge(
-      scopeType === "quotas" ? config.rateLimit : config.speedLimit,
-      acc[endp]
-    );
     return acc;
   }, {});
+}
+
+function expressPath(path){
+  return path.replace(/{/g, ':').replace(/}/g, '');
 }
